5765f77e24f0ff37922b0baf2b53a46751611f45
DesmondAde1993 <140061312+AdeDesmond@users.noreply.github.com>
2024-04-16 08:30:39 +0800

    name added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,16 @@ export const dynamic = "force-dynamic";
 //38 mins
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const posts = await db.query.posts.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
         {[...posts, ...posts, ...posts].map((image, index) => (
-          <div key={image.id + " " + index} className=" w-48">
+          <div key={image.id + " " + index} className=" flex w-48 flex-col">
             <Image src={image.url} alt="image" width={300} height={60} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
