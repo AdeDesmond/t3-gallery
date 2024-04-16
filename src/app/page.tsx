b87b76677677c0e7668993ed7bf135d6a933da0a
DesmondAde1993 <140061312+AdeDesmond@users.noreply.github.com>
2024-04-16 08:26:16 +0800

    use vercel db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
 import Image from "next/image";
 import { db } from "~/server/db";
+export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/de90b2cb-0fbc-41ec-af1b-a54327558723-245n.png",
-  "https://utfs.io/f/3ec5294d-9fed-438c-83cf-0eea97a73f13-pgh5fl.jpg",
-  "https://utfs.io/f/10852a41-db18-49a9-90f5-0d3bd9581135-1z743.jpg",
-  "https://utfs.io/f/5ceeb73e-7b1d-44af-96b6-0a8d83128fd9-1z748.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+//38 mins
 
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {[...posts, ...posts, ...posts].map((image, index) => (
           <div key={image.id + " " + index} className=" w-48">
             <Image src={image.url} alt="image" width={300} height={60} />
           </div>
