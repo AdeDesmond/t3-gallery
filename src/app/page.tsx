2fa7a7618f6a1b136b1b1254809e32654bda3025
DesmondAde1993 <140061312+AdeDesmond@users.noreply.github.com>
2024-04-16 09:00:19 +0800

    protect the image route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,35 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 //38 mins
 
-export default async function HomePage() {
+async function Images() {
   const posts = await db.query.posts.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...posts, ...posts, ...posts].map((image, index) => (
+        <div key={image.id + " " + index} className=" flex w-48 flex-col">
+          <Image src={image.url} alt="image" width={300} height={60} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...posts, ...posts, ...posts].map((image, index) => (
-          <div key={image.id + " " + index} className=" flex w-48 flex-col">
-            <Image src={image.url} alt="image" width={300} height={60} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">Please sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
